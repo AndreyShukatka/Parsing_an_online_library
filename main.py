1a314ab1b20487d12de08820c3b1c46bfe6f6be7
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-08-21 20:06:47 +0300

    Добавил args

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+import argparse
 import requests
 import os
 import pathlib
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin, urlsplit
+
+
+def input_parsing_command_line():
+    parser = argparse.ArgumentParser(
+        description='Программа скачивает книги с сайта https://tululu.org/'
+    )
+    parser.add_argument(
+        '--start_id',
+        help='укажите id с которого нужно начать скачивать',
+        default='1',
+        type=int
+    )
+    parser.add_argument(
+        '--end_id',
+        help='укажите id до которого нужно начать скачивать',
+        default='10',
+        type=int
+    )
+    args = parser.parse_args()
+    return args
 
 
 def request_tululu(url):
@@ -17,7 +38,7 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def parse_a_book(book_url):
+def parse_book_page(book_url):
     response = request_tululu(book_url)
     soup = BeautifulSoup(response.text, 'lxml')
     name_book, book_author = soup.find('h1').text.split(sep='::')
@@ -26,9 +47,10 @@ def parse_a_book(book_url):
     book_ganres = soup.find_all('span', class_='d_book')
     return name_book, book_cover, all_comment, book_ganres
 
+
 def download_image(id, book_url):
-    img_url = urljoin(url, parse_a_book(book_url)[1])
-    name_img = sanitize_filename(parse_a_book(book_url)[0]).strip()
+    img_url = urljoin(url, parse_book_page(book_url)[1])
+    name_img = sanitize_filename(parse_book_page(book_url)[0]).strip()
     folder_name = os.path.join('img', f'{id}.{name_img}{extract_file_extension(img_url)}')
     pathlib.Path('img').mkdir(
         parents=True,
@@ -49,26 +71,26 @@ def search_comment(all_comment):
         print(comment.find('span').text)
 
 
-def search_ganres(book_ganres):
-    bo_ganres = []
-    for ganres in book_ganres:
-        ganres = ganres.find_all('a')
-        for ganre in ganres:
-            bo_ganres.append(ganre.text)
-    return bo_ganres
+def search_genres(book_genres):
+    bo_genres = []
+    for genres in book_genres:
+        genres = genres.find_all('a')
+        for genre in genres:
+            bo_genres.append(genre.text)
+    return bo_genres
 
 
-def download_txt(url):
-    for id in range(1,11):
+def download_txt(start_id, end_id, url):
+    for id in range(start_id,end_id):
         try:
             book_url = f'{url}b{id}/'
             response = request_tululu(book_url)
             check_for_redirect(response)
             download_image(id, book_url)
-            name_book = sanitize_filename(parse_a_book(book_url)[0]).strip()
+            name_book = sanitize_filename(parse_book_page(book_url)[0]).strip()
             print('Заголовок:', name_book)
-            book_ganres = parse_a_book(book_url)[3]
-            print(search_ganres(book_ganres))
+            book_genres = parse_book_page(book_url)[3]
+            print(search_genres(book_genres))
             # all_comment = parse_a_book(book_url)[2]
             # search_comment(all_comment)
             folder_name = os.path.join('books',f'{id}.{name_book}.txt')
@@ -86,4 +108,7 @@ def download_txt(url):
 
 if __name__ == '__main__':
     url = 'https://tululu.org/'
-    download_txt(url)
+    args = input_parsing_command_line()
+    start_id = args.start_id
+    end_id = args.end_id + 1
+    download_txt(start_id, end_id, url)
