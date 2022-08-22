dd4b3ed9b82945c53b2fd673ab56a40e7542499c
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-08-22 22:05:55 +0300

    Поменял find на select

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -11,7 +11,7 @@ def parse_category(url):
         fantasy_url = f'{url}l55/{id}'
         response = requests.get(fantasy_url)
         soup = BeautifulSoup(response.text, 'lxml')
-        all_book_urls = soup.find_all('table', class_='d_book')
-        for book_url in all_book_urls:
-            books_urls.append(urljoin(url, book_url.find('a')['href']))
+        all_books_urls = soup.select('.d_book')
+        for book_url in all_books_urls:
+            books_urls.append(urljoin(url, book_url.select_one('a')['href']))
     return books_urls
