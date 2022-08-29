b8d8e416979224e8afc5c7b6c1e83e0e6b37c214
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-08-29 18:15:32 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,26 +1,34 @@
 import os
 import json
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from livereload import Server
 
-path_file = os.path.join('library', 'books_pages.json')
 
-def get_book_pages():
+def get_book_pages(path_file):
     with open(path_file, "r", encoding='utf-8') as books:
         books_pages = books.read()
     books = json.loads(books_pages)
     return books
 
-env = Environment(
-    loader=FileSystemLoader('library'),
-    autoescape=select_autoescape(['html'])
-)
-template = env.get_template('template.html')
-rendered_page = template.render(
-    books = get_book_pages()
-)
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('template'),
+        autoescape=select_autoescape(['html'])
+    )
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        books=get_book_pages(path_file)
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    print('asdasdasda')
+
+
+if __name__ == '__main__':
+    path_file = os.path.join('library', 'books_pages.json')
+    on_reload()
+    server = Server()
+    server.watch('template/template.html', on_reload)
+    server.serve(root='.')
+    
